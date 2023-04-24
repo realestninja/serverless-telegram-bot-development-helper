113c69bc666227c9b317b5dd29f8e77b24d5a576
Nikhil Nawgiri <the@realest.ninja>
2023-04-24 17:28:16 +0200

    Forward received message to wrangler's localhost

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
+import axios from 'axios';
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import * as dotenv from "dotenv"
 
 dotenv.config()
+
+const WRANGLER_LOCAL = "http://localhost:8787";
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.on(message(), (ctx) => {
-	// console.log(ctx)
-	const { message } = ctx.update;
-	const { id } = message.from;
-	const { text } = message;
-
-  bot.telegram.sendMessage(id, "hello world");
+  if (ctx.hasOwnProperty("update")) axios.post(WRANGLER_LOCAL, ctx.update);
 });
-bot.launch();
 
+bot.launch();
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
