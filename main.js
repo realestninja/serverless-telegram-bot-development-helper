53f64f164221f02d3ac595754495ab030e17356e
Nikhil Nawgiri <the@realest.ninja>
2023-04-25 08:46:00 +0200

    Refactor

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
+import { config } from "dotenv";
 import axios from 'axios';
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
-import * as dotenv from "dotenv"
+import { PORT } from "./constants.js";
 
-dotenv.config()
+config()
 
-const WRANGLER_LOCAL = "http://localhost:8787";
+const WRANGLER_LOCAL = `http://localhost:${PORT}`;
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.on(message(), (ctx) => {
-  if (ctx.hasOwnProperty("update")) axios.post(WRANGLER_LOCAL, ctx.update);
+  if (Object.prototype.hasOwnProperty.call(ctx, "update")) {
+    axios.post(WRANGLER_LOCAL, ctx.update);
+  }
 });
 
 bot.launch();
